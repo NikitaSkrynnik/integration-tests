890c8b000de164fa0cb863edea7a58f88f321958
Nikita Skrynnik <nikita.skrynnik@xored.com>
2023-06-07 19:18:22 +0700

    enable prefetch suite
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- extensions/base/suite.go
+++ extensions/base/suite.go
@@ -79,8 +79,8 @@ func (s *Suite) SetupSuite() {
 		fmt.Sprintf("https://api.github.com/repos/%v/contents/apps?ref=%v", repo, version),
 	}
 
-	// s.prefetch.SetT(s.T())
-	// s.prefetch.SetupSuite()
+	s.prefetch.SetT(s.T())
+	s.prefetch.SetupSuite()
 
 	s.nsMonitorCtx, s.nsMonitorCancel = context.WithCancel(context.Background())
 	logs.MonitorNamespaces(s.nsMonitorCtx)
