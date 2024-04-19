b1b9a1d0ec08a967ce84c5f0f276344bccf0024d
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-04-19 14:19:16 +0700

    tests
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- suites/interdomain/two_cluster_configuration/basic/suite.gen.go
+++ suites/interdomain/two_cluster_configuration/basic/suite.gen.go
@@ -36,8 +36,8 @@ func (s *Suite) SetupSuite() {
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG1 delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG1 delete ns nsm-system`)
 		r.Run(`kubectl --kubeconfig=$KUBECONFIG2 delete mutatingwebhookconfiguration nsm-mutating-webhook` + "\n" + `kubectl --kubeconfig=$KUBECONFIG2 delete ns nsm-system`)
 	})
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster1?ref=89c1b4246f503c6f6ccc1daa53cb20d872b49e24`)
-	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k https://github.com/networkservicemesh/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster2?ref=89c1b4246f503c6f6ccc1daa53cb20d872b49e24`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 apply -k ../../../../../../../../../../Users/user/repos/NSM/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster1`)
+	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 apply -k ../../../../../../../../../../Users/user/repos/NSM/deployments-k8s/examples/interdomain/two_cluster_configuration/basic/cluster2`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG1 wait --for=condition=ready --timeout=1m pod -n nsm-system -l app=admission-webhook-k8s`)
 	r.Run(`kubectl --kubeconfig=$KUBECONFIG2 wait --for=condition=ready --timeout=1m pod -n nsm-system -l app=admission-webhook-k8s`)
 }
